5b4b30e2826b70b5392e7c6adad0d724584cab8e
Daniel <dperezterron19@gmail.com>
2025-04-06 04:23:35 +0200

    Physical animation ADDED
    He utilizado una extension externa para los ConfigurableJoints para evitar aprender que coi es un Quaternion.
    El autor original es M.Stevenson,
    github del codigo aqui: https://gist.github.com/mstevenson/7b85893e8caf5ca034e6

--- Assets/Scripts/CharacterScripts/charAnimationScript.cs
+++ Assets/Scripts/CharacterScripts/charAnimationScript.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 
 public class charAnimationScript : MonoBehaviour
 {
-  
+    public Transform target;
+    public ConfigurableJoint joint;
+
+    private Quaternion startingRotation;
+
+    private void Start()
+    {
+        startingRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        joint.SetTargetRotationLocal(target.rotation, startingRotation);
+    }
 }
