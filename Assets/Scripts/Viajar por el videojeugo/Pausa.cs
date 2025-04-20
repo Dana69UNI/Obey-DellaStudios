c4571e2a4deae3c13425acbbd83b2197e73fce95
Marcos2611 <155254634+Marcos2611@users.noreply.github.com>
2025-04-20 15:47:34 +0200

    menu de pausa a medias

--- Assets/Scripts/Viajar por el videojeugo/Pausa.cs
+++ Assets/Scripts/Viajar por el videojeugo/Pausa.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pausa : MonoBehaviour
+public class Codigo_Pausa : MonoBehaviour
 {
-    public GameObject MenuPausa;
+    public GameObject ObjetoMenuPausa;
     public bool Pausa = false;
 
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if(Pausa == false)
+            if (Pausa == false)
             {
-                MenuPausa.SetActive(true);
+                ObjetoMenuPausa.SetActive(true);
                 Pausa = true;
+
+                Time.timeScale = 0;
             }
+            else
+            {
+                ObjetoMenuPausa.SetActive(false);
+                Time.timeScale = 1f;
+                Pausa = false;
+            }
+
         }
     }
 }
