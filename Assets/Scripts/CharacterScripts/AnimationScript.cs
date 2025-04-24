8ef22c37aee100b74be004e3ad16baeaad5ce5b8
Dana69UNI <126713261+Dana69UNI@users.noreply.github.com>
2025-04-24 13:56:22 +0200

    Cambios Arreglar Merge

--- Assets/Scripts/CharacterScripts/AnimationScript.cs
+++ Assets/Scripts/CharacterScripts/AnimationScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimationScript : MonoBehaviour
 {
     Animator _animator;
+    float jumpTimer=0f;
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -18,5 +19,22 @@ public class AnimationScript : MonoBehaviour
     private void OnMovementRelease()
     {
         _animator.SetBool("IsMoving", false);
+    }
+
+    private void OnJump()
+    {
+        StartCoroutine(jumpCD());
+    }
+
+    private IEnumerator jumpCD()
+    {
+        
+        while (jumpTimer < 1f)
+        {
+            _animator.SetBool("IsJumping", true);
+            jumpTimer += Time.deltaTime;
+            yield return null;
+        }
+        _animator.SetBool("IsJumping", false);
     }
 }
