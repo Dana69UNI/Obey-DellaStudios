01e9d632b746980f17b9b64a67713e31cd5f116b
Daniel <dperezterron19@gmail.com>
2025-06-07 02:01:02 +0200

    Engranajes Rework y low poly
    He cambiado el script de engranaje para que lo tenga el punto y no el engranaje correcto, para evitar multiples respuestas correctas. Y he puesto los engranajes lowpoly que estaban los high poly

--- Assets/Scripts/Puzzle/Engranaje.cs
+++ Assets/Scripts/Puzzle/Engranaje.cs
@@ -2,37 +2,41 @@ using UnityEngine;
 
 public class Engranaje : MonoBehaviour
 {
-    public Animator animatorEngranaje;
-    public Animator animatorPuerta;
+    private Animator animatorEngranaje;
+    //public Animator animatorPuerta;
     public Animator animatorEngranaje2;
     public Animator animatorEngranaje3;
+    public GameObject EngranajeCorrecto;
+    public GameObject Llave;
 
     private Rigidbody rb;
     private bool yaPegado = false;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = EngranajeCorrecto.GetComponent<Rigidbody>();
+        animatorEngranaje = EngranajeCorrecto.GetComponent<Animator>();
+        animatorEngranaje.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (yaPegado) return;
 
-        if (other.CompareTag("PuntoEngranaje"))
+        if (other.CompareTag("Engranaje"))
         {
             // Paramos física para que no interfiera
             rb.isKinematic = true;
             rb.useGravity = false;
 
             // Teletransportamos al punto exacto
-            transform.position = other.transform.position;
-            transform.rotation = other.transform.rotation;
+            other.transform.position = transform.position;
+            other.transform.rotation = transform.rotation;
 
             yaPegado = true;
-
+            animatorEngranaje.enabled = true;
             animatorEngranaje.SetTrigger("rotar");
-            animatorPuerta.SetTrigger("abrir");
+            //animatorPuerta.SetTrigger("abrir");
             animatorEngranaje2.SetTrigger("rotar");
             animatorEngranaje3.SetTrigger("rotar");
 
